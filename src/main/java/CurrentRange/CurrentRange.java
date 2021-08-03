8a05a300b4b0fb2d5ced0b5b8261caca840ccc11
PavithraMuralidharan <77607394+PavithraMuralidharan@users.noreply.github.com>
2021-08-03 14:35:41 +0530

    Add files via upload

--- src/main/java/CurrentRange/CurrentRange.java
+++ src/main/java/CurrentRange/CurrentRange.java
@@ -5,56 +5,55 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class CurrentRange
-{
-  private List<Integer> currentSampleList;
-  public Map<String, String> detectedRange;
+public class CurrentRange {
+	private List<Integer> currentSampleList;
+	Map<String, Integer> ranges = new HashMap<>();
 
-  public CurrentRange(List<Integer> currentSampleList)
-  {
-    this.currentSampleList = currentSampleList;
-  }
-
-  public List<Integer> getSortedRanges()
-  {
-    Collections.sort(this.currentSampleList);
-    return this.currentSampleList;
-  }
-
-  public boolean isEmpty()
-  {
-    return this.currentSampleList.isEmpty();
-  }
-  
-  public boolean isRangeContainsNull()
-  {
-	  return this.currentSampleList.contains(null);
-  }
-  
-  public boolean isRangeNull()
-  {
-	  return this.currentSampleList == null;
-  }
-
-  public void detectRange()
-  {
-	int start=0;
-    int readings = 0;
-    List<Integer> sortedList = getSortedRanges();
-    Map<String,String> ranges = new HashMap<>();
-    if(!isRangeNull() && !isRangeContainsNull())
-    {
-    	ranges.put("Range", "Reading");
-		for (int i = 1; i < sortedList.size(); i++) {
-			readings++;
-			if (sortedList.get(i - 1) + 1 != sortedList.get(i) && sortedList.get(i - 1) != sortedList.get(i)) {
-				ranges.put(String.valueOf(sortedList.get(start) - sortedList.get(i - 1)), String.valueOf(readings));
-				readings = 0;
-				start = i;
-			}
-		}
-		ranges.put(String.valueOf(sortedList.get(start) - sortedList.get(sortedList.size() - 1)), String.valueOf(readings));
-		this.detectedRange = ranges;
+	public CurrentRange(List<Integer> currentSampleList) {
+		this.currentSampleList = currentSampleList;
 	}
-  }
+
+	public List<Integer> getSortedRanges() {
+		Collections.sort(currentSampleList);
+		return currentSampleList;
+	}
+
+	public boolean isEmpty() {
+		return currentSampleList.isEmpty();
+	}
+
+	public boolean isRangeContainsNull() {
+		return currentSampleList.contains(null);
+	}
+
+	public boolean isRangeNull() {
+		return this.currentSampleList == null;
+	}
+
+	public Map<String, Integer> detectRange() {
+		List<Integer> sortedList = getSortedRanges();
+
+		if (isRangeNull() && isRangeContainsNull())
+			return null;
+
+		int count = 1;
+		int minRange = sortedList.get(0);
+		int maxRange = sortedList.get(0);
+
+		for (int i = 1; i < sortedList.size(); ++i) {
+			if ((sortedList.get(i) - sortedList.get(i - 1)) < 2) {
+				++count;
+			}
+
+			else {
+				ranges.put(minRange + "-" + maxRange, count);
+				minRange = maxRange = sortedList.get(i);
+				count = 1;
+
+			}
+			maxRange = sortedList.get(i);
+		}
+		ranges.put(minRange + "-" + maxRange, count);
+		return ranges;
+	}
 }
